210a346242e1896abfc3df256f148084e16b9765
jinwook-song <wlsdnr129@naver.com>
2022-01-09 15:59:38 +0900

    update README.md

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,20 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Chart from "./routes/Chart";
-import Coin from "./routes/Coin";
-import Coins from "./routes/Coins";
-import Price from "./routes/Price";
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Chart from './routes/Chart';
+import Coin from './routes/Coin';
+import Coins from './routes/Coins';
+import Price from './routes/Price';
 
 function Router() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/:coinId/*" element={<Coin />}>
-          <Route path="chart" element={<Chart />} />
-          <Route path="price" element={<Price />} />
+        <Route path='/:coinId/*' element={<Coin />}>
+          <Route path='chart' element={<Chart />} />
+          <Route path='price' element={<Price />} />
         </Route>
-        <Route path="/" element={<Coins />} />
+        <Route path='/' element={<Coins />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
